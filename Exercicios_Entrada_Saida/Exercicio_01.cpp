401398d98bd3abd3e7e4883155aca847e8618507
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-08-21 20:54:56 -0300

    Add files via upload

--- Exercicios_Entrada_Saida/Exercicio_01.cpp
+++ Exercicios_Entrada_Saida/Exercicio_01.cpp
@@ -1,15 +1,33 @@
-/*
-primeiro programa
+/*-------------------------------------
+Mostrar operações aritméticas na tela
 ok
-*/
+--------------------------------------*/
 
-//bibliotecas uteis para o funcionamento do programa 
+//bibliotecas necessárias para o funcionamento do programa
 #include <stdio.h>
 #include <stdlib.h>
-
-int main () //inicio do programa 
-{
-	system("cls"); //limpar a tela 
-	printf("Hello World\n"); //mostra uma mensagem na tela e quebra a linha 
-	system("pause");// pausa o programa e espera um enter do usuÃ¡rio para finalizar 
-}
+#include <locale.h>
+//método príncipal(inicio do programa)
+int main()
+{//inicio do código
+	setlocale(LC_ALL,"Portuguese");
+	//declaração de variaveis 
+	int a,b;
+	float x,y;
+	//atribuição de valores a variaveis 
+	a = 1;
+	b = a + 2;
+	x = a / b;
+	b = b + 1;
+	a = a - 1;
+	y = b / 2;
+	a = a + 2;
+	y = b % (a + 1);
+	//mostrar valores das atribuições na tela 
+	printf("%d\n",a);
+	printf("%d\n",b);
+	printf("%.1f\n",x);
+	printf("%.1f\n",y);
+	//pausa antes do termino do programa 
+	system("pause");
+}//fim do código e do programa
