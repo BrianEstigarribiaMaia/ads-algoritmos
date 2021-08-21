e6ac784423d485e2bc8dc516d55a5bc9b3d789ff
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-08-21 20:58:43 -0300

    Add files via upload

---
+++ Exercicios_Entrada_Saida/Exercicio_01.cpp
@@ -0,0 +1,15 @@
+/*
+primeiro programa
+ok
+*/
+
+//bibliotecas uteis para o funcionamento do programa 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () //inicio do programa 
+{
+	system("cls"); //limpar a tela 
+	printf("Hello World\n"); //mostra uma mensagem na tela e quebra a linha 
+	system("pause");// pausa o programa e espera um enter do usuário para finalizar 
+}
